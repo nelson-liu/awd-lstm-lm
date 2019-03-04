e726345c95d0742e3d81886a3b6a65af22c11ec5
Nelson Liu <nfliu@cs.washington.edu>
2019-03-04 15:03:50 -0500

    Add script to generate completions from awd lstm

--- generate.py
+++ generate.py
@@ -6,73 +6,68 @@
 ###############################################################################
 
 import argparse
+import json
 
 import torch
 from torch.autograd import Variable
+from tqdm import tqdm
 
 import data
 
-parser = argparse.ArgumentParser(description='PyTorch PTB Language Model')
+parser = argparse.ArgumentParser(description='Generate from PyTorch AWD LSTM LM')
 
 # Model parameters.
-parser.add_argument('--data', type=str, default='./data/penn',
-                    help='location of the data corpus')
-parser.add_argument('--model', type=str, default='LSTM',
-                    help='type of recurrent net (LSTM, QRNN)')
-parser.add_argument('--checkpoint', type=str, default='./model.pt',
+parser.add_argument('--corpus-path', type=str, required=True,
+                    help=('Location of the data corpus. Required to '
+                          'recalculate the vocab.'))
+parser.add_argument('--model-path', type=str, required=True,
                     help='model checkpoint to use')
-parser.add_argument('--outf', type=str, default='generated.txt',
-                    help='output file for generated text')
-parser.add_argument('--words', type=int, default='1000',
-                    help='number of words to generate')
-parser.add_argument('--seed', type=int, default=1111,
-                    help='random seed')
+parser.add_argument('--data-path', type=str, required=True,
+                    help='Path to the completion examples.')
+parser.add_argument('--output-path', type=str, required=True,
+                    help='Path to write the output completion examples.')
 parser.add_argument('--cuda', action='store_true',
                     help='use CUDA')
-parser.add_argument('--temperature', type=float, default=1.0,
-                    help='temperature - higher will increase diversity')
-parser.add_argument('--log-interval', type=int, default=100,
-                    help='reporting interval')
 args = parser.parse_args()
 
 # Set the random seed manually for reproducibility.
-torch.manual_seed(args.seed)
 if torch.cuda.is_available():
     if not args.cuda:
         print("WARNING: You have a CUDA device, so you should probably run with --cuda")
     else:
         torch.cuda.manual_seed(args.seed)
 
-if args.temperature < 1e-3:
-    parser.error("--temperature has to be greater or equal 1e-3")
-
-with open(args.checkpoint, 'rb') as f:
-    model = torch.load(f)
+with open(args.model_path, 'rb') as f:
+    model = torch.load(f, map_location=lambda storage, loc: storage)[0]
 model.eval()
-if args.model == 'QRNN':
-    model.reset()
 
 if args.cuda:
     model.cuda()
 else:
     model.cpu()
 
-corpus = data.Corpus(args.data)
-ntokens = len(corpus.dictionary)
-hidden = model.init_hidden(1)
-input = Variable(torch.rand(1, 1).mul(ntokens).long(), volatile=True)
-if args.cuda:
-    input.data = input.data.cuda()
+corpus = data.Corpus(args.corpus_path)
+initial_hidden = model.init_hidden(1)
+unk_index = corpus.dictionary.word2idx["<UNK>"]
 
-with open(args.outf, 'w') as outf:
-    for i in range(args.words):
-        output, hidden = model(input, hidden)
-        word_weights = output.squeeze().data.div(args.temperature).exp().cpu()
-        word_idx = torch.multinomial(word_weights, 1)[0]
-        input.data.fill_(word_idx)
-        word = corpus.dictionary.idx2word[word_idx]
 
-        outf.write(word + ('\n' if i % 20 == 19 else ' '))
+def get_next_word(previous_str, topk=10):
+    split_previous_str = previous_str.split(" ")
+    previous_str_indices = [corpus.dictionary.word2idx.get(word, unk_index) for word in
+                            split_previous_str]
+    input = Variable(torch.LongTensor(previous_str_indices).unsqueeze(-1), volatile=True)
+    if args.cuda:
+        input.data = input.data.cuda()
+    output, _ = model(input, initial_hidden)
+    word_weights = model.decoder(output)
+    best_weights, best_indices = word_weights.topk(topk)
+    best_words = [corpus.dictionary.idx2word[x] for x in best_indices[-1].data]
+    best_weights = best_weights[-1].data.tolist()
+    return {"words": best_words, "weights": best_weights}
 
-        if i % args.log_interval == 0:
-            print('| Generated {}/{} words'.format(i, args.words))
+
+with open(args.data_path) as data_file, open(args.output_path, "w") as output_file:
+    for line in tqdm(data_file):
+        instance = json.loads(line)
+        next_word_output = get_next_word(" ".join(instance["prefix"]))
+        output_file.write("{}\n".format(json.dumps(next_word_output)))
